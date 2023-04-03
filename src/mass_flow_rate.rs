c1f6c07474dc13277a9047f2a68eb12afce0cfdc
Philipp Dobler <binksmrs@gmail.com>
2023-04-03 15:26:35 +0200

    Fix mass flow rate units.

--- src/mass_flow_rate.rs
+++ src/mass_flow_rate.rs
@@ -5,8 +5,8 @@ use crate::prelude::*;
 #[quantity(Mass / Duration)]
 #[ref_unit(Kilogram_per_Second, "kg/s", KILO, "Reference unit of quantity `MassFlowRate`")]
 #[unit(Tonne_per_Second, "t/s", MEGA, 1e3)]
-#[unit(Tonne_per_Month, "t/month", MEGA, 2.592e6)]
-#[unit(Kilotonne_per_Month, "kt/month", GIGA, 2.592e9)]
-#[unit(Megatonne_per_Month, "Mt/month", TERA, 2.592e12)]
-#[unit(Gigatonne_per_Month, "Gt/month", PETA, 2.592e15)]
+#[unit(Tonne_per_Month, "t/month", MEGA, 0.3858024691e-3)]
+#[unit(Kilotonne_per_Month, "kt/month", GIGA, 0.3858024691)]
+#[unit(Megatonne_per_Month, "Mt/month", TERA, 0.3858024691e3)]
+#[unit(Gigatonne_per_Month, "Gt/month", PETA, 0.3858024691e6)]
 pub struct MassFlowRate {}
